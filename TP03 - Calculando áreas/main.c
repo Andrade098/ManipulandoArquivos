0da963031906a48b341723bd2cf84e7236639d6c
Lucas Andrade Torres <127760173+Andrade098@users.noreply.github.com>
2023-09-25 16:26:10 -0300

    Update main.c

--- TP03 - Calculando áreas/main.c
+++ TP03 - Calculando áreas/main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definição do TAD Ponto
+
 typedef struct {
     float X;
     float Y;
 } Ponto;
 
-// Função para calcular a área do polígono
+
 double calcularArea(FILE *arquivo) {
     int n; // Número de vértices
     fscanf(arquivo, "%d", &n);
 
-    // Alocar espaço dinamicamente para armazenar os pontos
+   
     Ponto *vertices = (Ponto *)malloc(n * sizeof(Ponto));
 
-    // Verificar se a alocação de memória foi bem-sucedida
+  
     if (vertices == NULL) {
         printf("Erro ao alocar memória.\n");
         exit(1);
     }
 
-    // Ler as coordenadas dos vértices do arquivo
+   
     for (int i = 0; i < n; i++) {
         fscanf(arquivo, "%f %f", &vertices[i].X, &vertices[i].Y);
     }
@@ -33,7 +33,7 @@ double calcularArea(FILE *arquivo) {
         area += (vertices[i].X * vertices[j].Y) - (vertices[j].X * vertices[i].Y);
     }
 
-    area = 0.5 * abs(area); // Valor absoluto e divisão por 2
+    area = 0.5 * abs(area); 
 
     // Liberar a memória alocada dinamicamente
     free(vertices);
@@ -44,7 +44,7 @@ double calcularArea(FILE *arquivo) {
 int main() {
     FILE *arquivo;
 
-    // Solicitar o nome do arquivo de entrada
+
     char nomeArquivo[100];
     printf("Digite o nome do arquivo de entrada: ");
     scanf("%s", nomeArquivo);
@@ -56,10 +56,10 @@ int main() {
         return 1;
     }
 
-    // Calcular a área do polígono
+
     double area = calcularArea(arquivo);
 
-    // Exibir a área
+
     printf("A área do polígono é %.2lf\n", area);
 
     fclose(arquivo);
